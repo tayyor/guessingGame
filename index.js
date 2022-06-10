97229569aa8827a010fbb20b723cdcf4905a4cc9
tayyor <abdulhaphyz@gmail.com>
2022-06-10 21:50:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// getting username...
+const player = prompt("What's your name?");
+var guess;
+
+
+// function to generate random number
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    
+}
+ 
+// setting range as a function parameter....
+
+function range(a){
+     return randomNumber(1,2);
+}
+
+// player enters a number
+ guess = prompt(`${player} what number am I thinking of ... 1 or 2 ?`)
+
+// compare player entry to random number generated....
+if(guess!= range()){
+    alert(`loool, try again`)
+}else
+    alert(`That's right ... congratulations`);
